c9046d0159d24adbd4c812556f5f50bb900e757f
LeAnhTuan647 <92526269+AnhTuan0011@users.noreply.github.com>
2023-03-23 18:27:52 +0700

    Third

--- js/jsmain.js
+++ js/jsmain.js
@@ -21,3 +21,29 @@ $(document).ready(function() {
         $('ul.navbar-nav').slideToggle();
     });
 })
+
+// 
+
+$(document).ready(function() {
+    $('.slide__st__rooms').owlCarousel({
+        loop:true,
+        margin: 10,
+        nav:true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1000,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        responsive:{
+            0:{
+                items: 1
+            },
+            991:{
+                items: 2
+            },
+            1300:{
+                items: 2
+            }
+        }
+    })
+})
